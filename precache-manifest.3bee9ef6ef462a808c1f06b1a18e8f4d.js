a424434385fed0f71c8c49e6041f65692fcceb29
Hamza Ashfaq <hamzaashfaq7866@gmail.com>
2020-08-30 13:31:47 +0500

    Updates

---
+++ precache-manifest.3bee9ef6ef462a808c1f06b1a18e8f4d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5701de75d38779359c4f69e98e6ac442",
+    "url": "/React-Theme1/index.html"
+  },
+  {
+    "revision": "3cdcd2e073e91d1dd723",
+    "url": "/React-Theme1/static/css/main.ad817fcd.chunk.css"
+  },
+  {
+    "revision": "0d4b3415eb1e19b75eec",
+    "url": "/React-Theme1/static/js/2.fc32cebd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Theme1/static/js/2.fc32cebd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cdcd2e073e91d1dd723",
+    "url": "/React-Theme1/static/js/main.ffbb353b.chunk.js"
+  },
+  {
+    "revision": "d54eab10f62f22d3fc51",
+    "url": "/React-Theme1/static/js/runtime-main.14a381a2.js"
+  },
+  {
+    "revision": "901d5413bc742fea28c043575e2aae02",
+    "url": "/React-Theme1/static/media/Hamza.901d5413.jpg"
+  },
+  {
+    "revision": "ec8d42769a833f2a840a6e978421a600",
+    "url": "/React-Theme1/static/media/bg-1.ec8d4276.jpg"
+  },
+  {
+    "revision": "f02075fcd73592d551559d4d36862f2e",
+    "url": "/React-Theme1/static/media/hero-area.f02075fc.jpg"
+  }
+]);
